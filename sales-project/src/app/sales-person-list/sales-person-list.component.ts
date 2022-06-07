2ee88828845a6b3ce5bd93c64f55c23674540c26
Donald Donchi Fofack <donchidonald2000@gmail.com>
2022-06-07 19:50:11 +0200

    create an Array of objects

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -11,5 +12,13 @@ export class SalesPersonListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@luv2code.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@luv2code.com", 90000),
+    new SalesPerson("Mai", "Truong", "mai.truong@luv2code.com", 60000)
+  ]
 
 }
